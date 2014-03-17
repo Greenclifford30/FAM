a9fffd13c5a128845ccded7131f33c66cbf33e97
cg236_112816 <cg236@babbage.cs.missouri.edu>
2014-03-17 17:07:03 -0500

    Configuration Complete

---
+++ application/controllers/fam_controller.php
@@ -0,0 +1,34 @@
+<?php 
+
+class Fam_Controller extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	
+	
+
+	public function index($page = 'home')
+	{
+		$data['title'] = $page;
+
+		$this->load->view('templates/header', $data);
+		$this->load->view('pages/home');
+		$this->load->view('templates/footer');
+	}
+    
+}
+
+?>
