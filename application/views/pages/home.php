9d05c22e00f25dfcc451a9501dbf126ded4ef439
Clifford Green <Greenclifford30@gmail.com>
2014-03-19 18:18:27 -0500

    Working with foundation

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -3,12 +3,12 @@
 	<h1>Welcome to CodeIgniter!</h1>
 
 	<div class = "small -2 large - 4 columns">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p class = "hide-for-small-only">The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
+		<p class = "hide-for-medium-up">If you would like to edit this page you'll find it located at:</p>
 		<code>application/views/welcome_message.php</code>
 
-		<p>The corresponding controller for this page is found at:</p>
+		<p class = "hide-for-large-up">The corresponding controller for this page is found at:</p>
 		<code>application/controllers/welcome.php</code>
 
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,12 +9,30 @@
 		echo script_tag('js/vendor/modernizr.js');
 		echo script_tag('js/vendor/placeholder.js');
 		echo script_tag('js/foundation/foundation.js');
-
-
-
+		echo script_tag('js/foundation/foundation.topbar.js')
 	?>
 </head>
 <body>
-	<h1>WELCOME</h1>
+
+<div class="contain-to-grid sticky" data-options="sticky_on: large">
+  <nav class="top-bar" data-topbar>
+  	<ul class = "title-area">
+    <li class = "name">
+    	<h1><a href="#">FAM</a></h1>
+    </li>
+
+    	<section class = "top-bar-section">
+    		<ul class = "right">
+    			<li class =  "has-dropdown"
+    			<a href="#" FAMILY & MONEY </a>
+    			<ul class = "drowpdown"> <a href = "#"> Events </a>
+    			</ul>
+    			<ul class = "dropdown"><a href = "#"> Bio </a>
+    			</ul>
+    			<ul class = "dropdown"<a href = "#">Tickets </a> </ul>
+    	</li>
+
+  </nav>
+</div>
 
  
