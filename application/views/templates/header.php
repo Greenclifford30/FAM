218285ac7cc2fef0e5290d3d72d7ff47941163ff
Clifford Green <Greenclifford30@gmail.com>
2014-03-19 18:44:40 -0500

    add ofcanvas.js

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,7 +9,8 @@
 		echo script_tag('js/vendor/modernizr.js');
 		echo script_tag('js/vendor/placeholder.js');
 		echo script_tag('js/foundation/foundation.js');
-		echo script_tag('js/foundation/foundation.topbar.js')
+		echo script_tag('js/foundation/foundation.offcanvas.js');
+		echo script_tag('js/foundation/foundation.topbar.js');
 	?>
 </head>
 <body>
@@ -26,26 +27,21 @@
         <h1 class="title">Family & Money</h1>
       </section>
 
-      <section class="right-small">
-        <a class="right-off-canvas-toggle menu-icon" ><span></span></a>
-      </section>
     </nav>
 
     <aside class="left-off-canvas-menu">
       <ul class="off-canvas-list">
-        <li><label>Family</label></li>
+        <li><label>The Family</label></li>
         <li><a href="#">Person 1</a></li>
-        ...
+        <li><a href ="#"> Person 2 </a></li>
+        <li><a href = "#"> Person 3 </a></li>
+        <li><label>Events</label></li>
+        <li>Calendar</li>
+        <li><label>Tickets</label></li>
       </ul>
     </aside>
 
-    <aside class="right-off-canvas-menu">
-      <ul class="off-canvas-list">
-        <li><label>Users</label></li>
-        <li><a href="#">Hari Seldon</a></li>
-        ...
-      </ul>
-    </aside>
+   
 
     <section class="main-section">
       <!-- content goes here -->
