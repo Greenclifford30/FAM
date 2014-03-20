966535c92539f5a3184b156cf960954c71d8eed2
Clifford Green <Greenclifford30@gmail.com>
2014-03-19 19:12:50 -0500

    Atempt 3

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,4 +1,3 @@
-
 <div class="off-canvas-wrap">
   <div class="inner-wrap">
     <nav class="tab-bar">
@@ -10,29 +9,21 @@
         <h1 class="title">Family & Money</h1>
       </section>
 
+     
     </nav>
 
-
- <section class="main-section">
-          
-     <aside class="left-off-canvas-menu">
+    <aside class="left-off-canvas-menu">
       <ul class="off-canvas-list">
-        <li><label>The Family</label></li>
-        <li><a href="#">Person 1</a></li>
-        <li><a href ="#"> Person 2 </a></li>
-        <li><a href = "#"> Person 3 </a></li>
-        <li><label>Events</label></li>
-        <li>Calendar</li>
-        <li><label>Tickets</label></li>
+        <li><label>Family</label></li>
+        <li><a href="#">The Psychohistorians</a></li>
+        ...
       </ul>
     </aside>
-    </section>
 
-  <a class="exit-off-canvas"></a>
 
-  </div>
-<div class = "row">
-	<h1>Welcome to CodeIgniter!</h1>
+    <section class="main-section">
+    <div class = "row">
+		<h1>Welcome to CodeIgniter!</h1>
 
 	<div class = "small -2 large - 4 columns">
 		<p class = "hide-for-small-only">The page you are looking at is being generated dynamically by CodeIgniter.</p>
@@ -48,4 +39,10 @@
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 </div>
+      <!-- content goes here -->
+    </section>
+
+  <a class="exit-off-canvas"></a>
+
+	</div>
 </div>
