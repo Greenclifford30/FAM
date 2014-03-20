dd8bf77bd78a717ddca2cac34c0238def8184e93
Clifford Green <Greenclifford30@gmail.com>
2014-03-19 21:38:05 -0500

    orbit test

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -26,22 +26,44 @@
 
 
     <section class="main-section">
+
     <div class = "row" id = "background">
-		<h1>Welcome to CodeIgniter!</h1>
+		
 
-	<div class = "small -2 large - 4 columns">
-		<p class = "hide-for-small-only">The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p class = "hide-for-medium-up">If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
+	<div class = "small - 12 columns" id = "wrapper">
+		<div class = "small 6 columns"
+		<ul class="example-orbit" data-orbit>
+ 		 <li>
+   			 <img src="../assets/img/examples/satelite-orbit.jpg" alt="slide 1" />
+   		 <div class="orbit-caption">
+  	    Caption One.
+  	  </div>
+ 	 </li>
+	  <li>
+	    <img src="../assets/img/examples/andromeda-orbit.jpg" alt="slide 2" />
+	    <div class="orbit-caption">
+	      Caption Two.
+	    </div>
+	  </li>
+	  <li>
+	    <img src="../assets/img/examples/launch-orbit.jpg" alt="slide 3" />
+	    <div class="orbit-caption">
+      Caption Three.
+  	  </div>
+	  </li>
+		</ul>
+		</div>
+		<div class = "small 6 columns">
 		<p class = "hide-for-large-up">The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
+		
 
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+
+		<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+		</div>
 	</div>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+	
 	</div>
     </section>
 
